eb5a383b7d56af511aae7257bd4ab2ea0c4b6c59
jdm365 <jdm365@georgetown.edu>
2022-08-30 08:31:48 -0400

    work

--- linear_regression.cpp
+++ linear_regression.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include <stdio.h>
 #include <vector>
+#include <fstream>
+#include <sstream>
+
+#include <stdio.h>
+#include <assert.h>
 
 using namespace std;
 
@@ -8,119 +12,103 @@ class LinearRegression {
 	vector<float> x;
 	vector<float> y;
 
-	float m;
-	float b;
+	float beta_0;
+	float beta_1;
 
-	float sum_xy;
-	float sum_x;
-	float sum_y;
-	float sum_x_square;
-	float sum_y_square;
-
-public:
-	regression() {
-		m=0;
-		b=0;
-		sum_xy=0;
-		sum_x=0;
-		sum_y=0;
-		sum_x_square=0;
-		sum_y_square=0;
-	}
-
-	void calculate_coefficient() {
-		float N = x.size();
-		float numerator   = N * sum_xy - sum_x * sum_y;
-		float denominator = N * sum_x_square - sum_x * sum_x;
-		m = numerator / denominator;
-	}
-
-
-	void calculate_intercept() {
-		float N = x.size();
-		float numerator   = sum_y * sum_x_square - sum_x * sum_xy;
-		float denominator = N * sum_x_square - sum_x * sum_x;
-		b = numerator / denominator;
-	}
-
-	int n_rows() {
-		return x.size();
-	}
-
-	float coefficient() {
-		if (b == 0)
-			calculate_coefficient();
-		return b;
-	}
-	
-	void print_line() {
-		if (m == 0 && b == 0) {
-			calculate_coefficient();
-			calculate_intercept();
-		}
-		cout << "The best fitting line is y = ";
-		cout << m << "x + " << b << endl;
-	}
-	
-	void get_input(int n) {
-		for (int i = 0; i < n; i++) {
-			char comma;
-			float x_i;
-			float y_i;
-			cin >> x_i >> comma >> y_i;
-			sum_xy += x_i * y_i;
-			sum_x += x_i;
-			sum_y += y_i;
-			sum_x_square += x_i * x_i;
-			sum_y_square += y_i * y_i;
-			x.push_back(x_i);
-			y.push_back(y_i);
-		}
-	}
-
-	void show_data {
-		for (int i = 0; i< 62; i++) {
-			printf("_");
+	public:
+		LinearRegression() {
+			beta_0 = 0;
+			beta_1 = 0;
 		}
 
-		printf("\n\n");
-		printf("|%15s%5s %15s%5s%20s\n", "X", "", "Y", "", "|");
-
-		for (int = 0; i < x.size(); i++) {
-			printf("|%20f %20f%20s\n", x[i], y[i], "|");
-		}
-
-		for (int i = 0; i< 62; i++) {
-			printf("_");
-		}
-		printf("\n\n");
-	}
-
-	float predict(float x) {
-		return m * x + b;
-	}
-
-	float mse() {
-		float error = 0;
-		for (int i = 0; i < x.size(); i++) {
-			error += ((predict(x[i]) - y[i]) * (predict(x[i]) - y[i]));
-		}
-		return error;
-	}
-
-	float get_residuals(float num) {
-		for (int i = 0; i < x.size(); i++) {
-			if (num == x[i]) {
-				return (y[i] - predict(x[i]));
+		void get_input(int n) {
+			for (int i = 0; i < n; i++) {
+				char comma;
+				float x_i;
+				float y_i;
+				cin >> x_i >> comma >> y_i;
+				x.push_back(x_i);
+				y.push_back(y_i);
 			}
+			assert (x.size() == y.size();
 		}
-		return 0;
-	}
+
+		float get_x_sum(int N) {
+			float x_sum = 0;
+			for (int idx = 0; idx < N; idx++) {
+				float x_sum += x[idx];
+			}
+			return x_sum;
+		}
+
+		float get_y_sum(int N) {
+			float y_sum = 0;
+			for (int idx = 0; idx < N; idx++) {
+				float y_sum += y[idx];
+			}
+			return y_sum;
+		}
+
+		float get_x_sqrd_sum(int N) {
+			float x_sqrd_sum = 0;
+			for (int idx = 0; idx < N; idx++) {
+				float x_sqrd_sum += x[idx] * x[idx];
+			}
+			return x_sqrd_sum;
+		}
+
+		float get_y_sqrd_sum(int N) {
+			float y_sqrd_sum = 0;
+			for (int idx = 0; idx < N; idx++) {
+				float y_sqrd_sum += y[idx] * y[idx];
+			}
+			return y_sqrd_sum;
+		}
+
+		float get_xy_sum(int N) {
+			float xy_sum = 0;
+			for (int idx = 0; idx < N; idx++) {
+				float xy_sum += x[idx] * y[idx];
+			}
+			return xy_sum;
+		}
+
+		void get_model() {
+			float N 		 = x.size();
+			float x_sum 	 = get_x_sum(N);
+			float y_sum 	 = get_y_sum(N);
+			float x_sqrd_sum = get_x_sqrd_sum(N);
+			float y_sqrd_sum = get_y_sqrd_sum(N);
+			float xy_sum	 = get_xy_sum(N);
+
+			float beta_0_num    = (y_sum * x_sqrd_sum) - (x_sum * xy_sum)
+			float beta_1_num    = N * sum_xy - sum_x * sum_y;
+			float denom		    = (N - 1) * x_sqrd_sum;
+
+			beta_0 = beta_0_num / denom;
+			beta_1 = beta_1_num / denom;
+
+			cout << "The best fitting line is y = ";
+			cout << beta_1 << "x + " << beta_0 << endl;
+		}
+
+		float predict(float x) {
+			return beta_1 * x + beta_0;
+		}
+
+		float mse(vector<float> predictions) {
+			float error = 0;
+			for (int idx = 0; idx < x.size(); idx++) {
+				error += (predictions - y[i]) * (predictions - y[i]));
+			}
+			return error;
+		}
 };
 
+
 int main() {
-	freopen("input.txt", "r", stdin);
-	regression reg;
+	freopen("input.csv", "r", stdin);
+	LinearRegression reg;
 
 	int n;
 	cin >> n;
@@ -131,4 +119,6 @@ int main() {
 	cout << "Predicted value at 2060 = " << reg.predict(2060) << endl;
 	cout << "MSE: " << reg.mse() << endl;
 	cout << "Error in 2050 = " << reg.get_residuals(2050) << endl;
+
+	return 0;
 }
