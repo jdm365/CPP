b8df87f81c792741b924fe9fdbb4114531de8b9f
jdm365 <jdm365@georgetown.edu>
2022-08-30 19:17:02 -0400

    Finished basic linear regression.

--- linear_regression.cpp
+++ linear_regression.cpp
@@ -1,42 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <sstream>
 
 #include <stdio.h>
 #include <assert.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class LinearRegression {
-	vector<float> x;
+	vector<vector<float>> X;
 	vector<float> y;
 
+	int   dims;
 	float beta_0;
-	float beta_1;
+	vector<float> beta_1;
 
 	public:
 		LinearRegression() {
+			dims   = 0;
 			beta_0 = 0;
-			beta_1 = 0;
 		}
 
-		void get_input(int n) {
-			for (int i = 0; i < n; i++) {
-				char comma;
-				float x_i;
-				float y_i;
-				cin >> x_i >> comma >> y_i;
-				x.push_back(x_i);
+		void read_input(string filename) {
+			ifstream file;
+			file.open(filename);
+			while (file.good()) {
+				string line;
+				getline(file, line, '\n');
+
+				if (line == "") {
+					break;
+				}
+				
+				dims = 0;
+				for (char c: line) {
+					if (c == ',') {
+						dims += 1;
+					}
+				}
+
+				stringstream check1(line);
+				string num;
+
+				for (int idx = 0; idx < dims; idx++) {
+					X.push_back(vector<float>());
+					getline(check1, num, ',');
+					float x_i = stof(num);
+					X[idx].push_back(x_i);
+				}
+
+				getline(check1, num, ',');
+				float y_i = stof(num);
+
 				y.push_back(y_i);
 			}
-			assert (x.size() == y.size();
+			file.close();
+
+			assert (X[0].size() == y.size());
 		}
 
-		float get_x_sum(int N) {
+		float get_x_sum(int N, int dim) {
 			float x_sum = 0;
 			for (int idx = 0; idx < N; idx++) {
-				float x_sum += x[idx];
+				x_sum += X[dim][idx];
 			}
 			return x_sum;
 		}
@@ -44,15 +71,15 @@ class LinearRegression {
 		float get_y_sum(int N) {
 			float y_sum = 0;
 			for (int idx = 0; idx < N; idx++) {
-				float y_sum += y[idx];
+				y_sum += y[idx];
 			}
 			return y_sum;
 		}
 
-		float get_x_sqrd_sum(int N) {
+		float get_x_sqrd_sum(int N, int dim) {
 			float x_sqrd_sum = 0;
 			for (int idx = 0; idx < N; idx++) {
-				float x_sqrd_sum += x[idx] * x[idx];
+				x_sqrd_sum += X[dim][idx] * X[dim][idx];
 			}
 			return x_sqrd_sum;
 		}
@@ -60,46 +87,63 @@ class LinearRegression {
 		float get_y_sqrd_sum(int N) {
 			float y_sqrd_sum = 0;
 			for (int idx = 0; idx < N; idx++) {
-				float y_sqrd_sum += y[idx] * y[idx];
+				y_sqrd_sum += y[idx] * y[idx];
 			}
 			return y_sqrd_sum;
 		}
 
-		float get_xy_sum(int N) {
+		float get_xy_sum(int N, int dim) {
 			float xy_sum = 0;
 			for (int idx = 0; idx < N; idx++) {
-				float xy_sum += x[idx] * y[idx];
+				xy_sum += X[dim][idx] * y[idx];
 			}
 			return xy_sum;
 		}
 
-		void get_model() {
-			float N 		 = x.size();
-			float x_sum 	 = get_x_sum(N);
+		void fit() {
+			float N 		 = y.size();
 			float y_sum 	 = get_y_sum(N);
-			float x_sqrd_sum = get_x_sqrd_sum(N);
 			float y_sqrd_sum = get_y_sqrd_sum(N);
-			float xy_sum	 = get_xy_sum(N);
 
-			float beta_0_num    = (y_sum * x_sqrd_sum) - (x_sum * xy_sum)
-			float beta_1_num    = N * sum_xy - sum_x * sum_y;
-			float denom		    = (N - 1) * x_sqrd_sum;
+			for (int dim = 0; dim < dims; dim++) {
+				float x_sum 	 = get_x_sum(N, dim);
+				float x_sqrd_sum = get_x_sqrd_sum(N, dim);
+				float xy_sum	 = get_xy_sum(N, dim);
 
-			beta_0 = beta_0_num / denom;
-			beta_1 = beta_1_num / denom;
+				float beta_1_num    = N * xy_sum - x_sum * y_sum;
+				float denom		    = (N - 1) * x_sqrd_sum;
+
+				beta_1.push_back(beta_1_num / denom);
+				
+				if (dim == (dims - 1)) {
+					float beta_0_num    = (y_sum * x_sqrd_sum) - (x_sum * xy_sum);
+					beta_0 = beta_0_num / denom;
+				}
+			}
 
 			cout << "The best fitting line is y = ";
-			cout << beta_1 << "x + " << beta_0 << endl;
+			for (int idx = 0; idx < beta_1.size(); idx++) {
+				cout << beta_1[idx] << " * x_" << idx << " + ";
+			}
+			cout << beta_0 << endl;
 		}
 
-		float predict(float x) {
-			return beta_1 * x + beta_0;
+		float predict(vector<float> X_pred) {
+			float pred = 0;
+			for (int idx = 0; idx < beta_1.size(); idx++) {
+				pred += beta_1[idx] * X_pred[idx];
+			}
+			pred += beta_0;
+			return pred;
 		}
 
-		float mse(vector<float> predictions) {
+		float mse() {
+			assert (X[0].size() == y.size());
+
 			float error = 0;
-			for (int idx = 0; idx < x.size(); idx++) {
-				error += (predictions - y[i]) * (predictions - y[i]));
+			for (int idx = 0; idx < beta_1.size(); idx++) {
+				float pred = predict(X[idx]);
+				error += (pred - y[idx]) * (pred - y[idx]);
 			}
 			return error;
 		}
@@ -107,18 +151,13 @@ class LinearRegression {
 
 
 int main() {
-	freopen("input.csv", "r", stdin);
-	LinearRegression reg;
+	LinearRegression regression;
 
-	int n;
-	cin >> n;
+	regression.read_input("data/iris_copy.csv");
 
-	reg.get_input(n);
-
-	reg.print_line();
-	cout << "Predicted value at 2060 = " << reg.predict(2060) << endl;
-	cout << "MSE: " << reg.mse() << endl;
-	cout << "Error in 2050 = " << reg.get_residuals(2050) << endl;
+	regression.fit();
+	float error = regression.mse();
+	cout << "Mean squared error: " << error << endl;
 
 	return 0;
 }
