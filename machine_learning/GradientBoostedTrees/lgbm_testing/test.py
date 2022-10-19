ff606bda2821fd01a81f285fd6c24f110f5e8acf
jdm365 <jdm365@georgetown.edu>
2022-10-19 17:51:02 -0400

    work

--- machine_learning/GradientBoostedTrees/lgbm_testing/test.py
+++ machine_learning/GradientBoostedTrees/lgbm_testing/test.py
@@ -2,13 +2,15 @@ import pandas as pd
 import lightgbm as lgb
 
 
-df = pd.read_csv('../data/housing_price_prediction_dataset.csv', header=None)
+## df = pd.read_csv('../data/housing_price_prediction_dataset.csv', header=None)
+df = pd.read_csv('../data/hpx10.csv', header=None)
 params = {
         'num_threads': 1,
-        'max_depth': 6,
+        'learning_rate': 0.5,
+        'max_depth': 5,
         'max_leaves': 1000,
         'min_gain_to_split': '0.0',
-        'force_col_wise': 'true'
+        'verbosity': '-1'
         }
 ds_params = {
         'use_missing': 'true',
@@ -19,4 +21,4 @@ ds_params = {
 train_cols = df.columns[:-1]
 target     = df.columns[-1]
 train_data = lgb.Dataset(df[train_cols], df[target], params=ds_params)
-model      = lgb.train(params, train_data, valid_sets=train_data, num_boost_round=25)
+model      = lgb.train(params, train_data, valid_sets=train_data, num_boost_round=50)
